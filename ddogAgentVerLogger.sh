83f16a7eb04eb2feb6b0d14c58f90bc70f7872dd
Ashley W <3814950+ashley-watson@users.noreply.github.com>
2023-12-08 09:17:01 -0700

    Update ddogAgentVerLogger.sh
    adjusting env tag

--- ddogAgentVerLogger.sh
+++ ddogAgentVerLogger.sh
@@ -6,7 +6,7 @@ Version=$(curl --silent "https://github.com/DataDog/datadog-agent/releases" | gr
 # Curl command
 curl -X POST "https://http-intake.logs.datadoghq.com/api/v2/logs" -H "Accept: application/json" -H "Content-Type: application/json" -H "DD-API-KEY: ${DD_API_KEY}" -d @- << EOF
   {
-    "ddtags": "env:KRDev,version:1.0,source:bash",
+    "ddtags": "env:Dev,version:1.0,source:bash",
     "host": "<hostname>",
     "ddsource": "Bash",
     "message": "Current Dataddog Agent version: $Version",
