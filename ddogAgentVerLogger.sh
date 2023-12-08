b42dbdc3ece377f5fead8237604b7c165b1df952
Ashley W <3814950+ashley-watson@users.noreply.github.com>
2023-12-08 09:15:38 -0700

    Create ddogAgentVerLogger.sh
    Gathers the current version of the Datadog Agent and submits it as a log to Datadog (Requires API & Application key)

---
+++ ddogAgentVerLogger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+while [ : ]
+do
+Version=$(curl --silent "https://github.com/DataDog/datadog-agent/releases" | grep -iE "DataDog/datadog-agent/tree/" | awk '{print $2}' | cut -c "35-40" | head -n1)
+
+# Curl command
+curl -X POST "https://http-intake.logs.datadoghq.com/api/v2/logs" -H "Accept: application/json" -H "Content-Type: application/json" -H "DD-API-KEY: ${DD_API_KEY}" -d @- << EOF
+  {
+    "ddtags": "env:KRDev,version:1.0,source:bash",
+    "host": "<hostname>",
+    "ddsource": "Bash",
+    "message": "Current Dataddog Agent version: $Version",
+    "version": "$Version",
+    "service": "agentChecker"
+  }
+EOF
+sleep 3600
+done
